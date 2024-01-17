07a3643ed82b3a731ace5c874e37e5a79005cfad
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2024-01-16 22:51:50 -0800

    build: improved rollup build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,60 @@
-const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
-const typescript = require("@rollup/plugin-typescript");
-const dts = require("rollup-plugin-dts");
+const json = require("@rollup/plugin-json");
+const resolve = require("@rollup/plugin-node-resolve");
+const external = require("rollup-plugin-peer-deps-external");
+const postcss = require("rollup-plugin-postcss");
+const sourceMaps = require("rollup-plugin-sourcemaps");
+const { terser } = require("rollup-plugin-terser");
+const typescript = require("rollup-plugin-typescript2");
+const pkg = require("./package.json");
 
-const packageJson = require("./package.json");
-
-module.exports = [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-    ],
+module.exports = {
+  input: `src/index.tsx`,
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+      plugins: [terser()],
+      exports: "auto",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true,
+      plugins: [terser()],
+      exports: "auto",
+    },
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+      exports: "auto",
+    },
+  ],
+  watch: {
+    include: "src/**",
   },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
-];
+  plugins: [
+    external(),
+    postcss({
+      modules: true,
+    }),
+    // Allow json resolution
+    json(),
+    // Compile TypeScript files
+    typescript({
+      useTsconfigDeclarationDir: true,
+      exclude: ["**/__tests__/**", "*.spec.*", "*.test.*"],
+      clean: true,
+    }),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+};
